3de02fe85f7b64ee4894435b89ae56c2c7d2165f
Manuel Barrera <manuelbarrera767@gmail.com>
2022-04-10 19:06:25 -0400

    services development

--- BarberHub/src/app/app.module.ts
+++ BarberHub/src/app/app.module.ts
@@ -10,7 +10,7 @@ import { AppComponent } from './app.component';
 import { AngularFireModule } from "@angular/fire/compat";
 import { AngularFireAuthModule } from "@angular/fire/compat/auth";
 import { AngularFireStorageModule } from '@angular/fire/compat/storage';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/compat/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from 'src/environments/environment';
 
@@ -19,7 +19,7 @@ import { environment } from 'src/environments/environment';
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFirestoreModule],
+    AngularFirestoreModule, AngularFireAuthModule],
   providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
   bootstrap: [AppComponent],
 })
---
+++ BarberHub/src/app/sel-bs/Ibarberia.ts
@@ -0,0 +1,12 @@
+export interface Barberia{
+    banco: String,
+    barbershop_id: String,
+    calificacion: String,
+    direccion: String,
+    email: String,
+    nombre: String,
+    nro_cuenta: String,
+    telefono_contacto: String,
+    telefono_pago: String,
+    tipo_cuenta: String
+}
