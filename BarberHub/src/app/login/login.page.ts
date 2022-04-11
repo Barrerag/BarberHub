b4eb972bfd85a3b413d8411ca994234e1e186798
Manuel Barrera <manuelbarrera767@gmail.com>
2022-04-11 16:50:32 -0400

    autenticación de usuario con mail/password y Google

--- BarberHub/src/app/login/login.page.ts
+++ BarberHub/src/app/login/login.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private authSvc: AuthService) { }
 
   ngOnInit() {
   }
 
+  async onLogin(email, password){
+    try{
+      const user = await this.authSvc.login(email.value, password.value);
+      if(user){
+        //verificar correo
+        console.log('email ->', email);
+        console.log('password ->', password)
+      }
+    }
+    catch(error){
+      console.log('Error ->', error);
+    }
+  }
+
+  async onLoginGoogle(){
+    try{
+      const user = await this.authSvc.loginGoogle();
+      if(user){
+        console.log('User ->', user);
+        
+      }
+    }
+    catch(error){
+      console.log('Error ->', error);
+    }
+  }
 }
