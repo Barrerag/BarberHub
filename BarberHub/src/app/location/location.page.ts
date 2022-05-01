73711b49ad460c274fba1e3b8748772277310ea4
Manuel Barrera <manuelbarrera767@gmail.com>
2022-05-01 00:25:00 -0400

    leaflet map added

--- BarberHub/src/app/location/location.page.ts
+++ BarberHub/src/app/location/location.page.ts
@@ -1,15 +1,26 @@
 import { Component, OnInit } from '@angular/core';
+import * as Leaflet from 'leaflet';
 
+ 
 @Component({
   selector: 'app-location',
   templateUrl: './location.page.html',
   styleUrls: ['./location.page.scss'],
 })
 export class LocationPage implements OnInit {
-
+  map: Leaflet.map;
   constructor() { }
 
   ngOnInit() {
+  }
+  ionViewDidEnter(){ this.leafletMap();}
+
+  leafletMap() {
+    this.map = Leaflet.map('mapId').setView([-36.592010, -72.121354], 16);
+    Leaflet.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(this.map);
+
+    Leaflet.marker([-36.592010, -72.121354]).addTo(this.map).bindPopup();
+
   }
 
 }
